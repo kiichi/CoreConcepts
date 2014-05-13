05f56f50501ab27d8c039225a3092c5df5b29005
Kiichi Takeuchi <kiichi.takeuchi@objectgraph.com>
2014-05-13 01:36:51 -0400

    rho square and line fitting example

--- class/week3/linear_regression_calc.m
+++ class/week3/linear_regression_calc.m
@@ -1,12 +1,27 @@
-%http://www.mathworks.com/help/matlab/data_analysis/linear-regression.html
-
-%calculate the coefficient of determination, r-square
-
+%
+% See Core Data Analysis Week 3 video.
+% 3.3 Correlation and determinacy coefficients: properties and meaning 
+% see equations.png and equations2.png!
+%
+% the second part is from the exable below.
+% http://www.mathworks.com/help/matlab/data_analysis/linear-regression.html
+%
+% calculate the coefficient of determination, r-square
+%
+% y = a * x + b
+% a is the slope
+%
+% r = a * std(x) / std(y)
+% r^2
+%
+% or 
+%
 % rsq = 1 - SSresid / SStotal
+% (see part 2 below)
 
 y=[20,32,28,44,76,65,97,121,140,160]
 x=1:length(y)
-[p,S,mu] = polyfit(x,y,1)
+%[p,S,mu] = polyfit(x,y,1)
 p = polyfit(x,y,1)
 
 %p shows 15.9333   -9.3333
@@ -15,9 +30,21 @@ p = polyfit(x,y,1)
 %a is 15.9333 and b is -9.3333
 %y = 15.9333 * x + -9.3333
 
-%Call polyval to use p to predict y, calling the result yfit:
+%-------- Shortcut for getting rho (part 1) ----------
+cof=p(1) * std(x) / std(y)
 
+%rho sqare = determinicy cof 
+% this should be matching rsq at the end!!!
+cof^2
+%--------------------------------------------
+
+%-------- Another way to calculate using polyval via fitting line (part 2) ----------
+%Call polyval to use p to predict y, calling the result yfit:
+% the polyval below does calculate
+% yfit =  p(1) * x + p(2);
+% yfit = a * x + b
 yfit = polyval(p,x);
+
 
 %Compute the residual values as a vector signed numbers:
 
--- class/week3/subsetting.m
+++ class/week3/subsetting.m
@@ -1,2 +1,4 @@
 %numeric is easy
 %rows=iris.VarName1<3
+rows=find(strncmp(iris.Iversicolor,'I.setosa',length('I.setosa')))
+setosa=iris(rows,:)
